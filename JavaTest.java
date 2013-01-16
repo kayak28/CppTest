9afb4de60f8132e8ad39cbc63ed9b6bec0ff885a
28kayak <kaya_ota@msn.com>
2013-01-16 15:46:07 -0800

    inital commit from ubuntu: NOT WORKgit add -i

---
+++ JavaTest.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+
+public class JavaTest 
+{
+	public static void main(String [] args)
+	{
+		Scanner scan = new Scanner(System.in);
+		String userName;
+		
+		userName = scan.nextLine();
+		System.out.println("Hello"+ userName +"!! Welcome!!!");
+	}
+
+
+}
